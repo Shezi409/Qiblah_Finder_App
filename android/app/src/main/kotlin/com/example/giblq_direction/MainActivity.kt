0a1e80d542add35489022f323fd481ebcb5256ef
Shezi409 <jamshezi777@gmail.com>
2023-10-19 17:22:34 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/giblq_direction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.giblq_direction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'Screen/qibla_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool hasPermission = false;
+
+  Future getPermission() async {
+    if (await Permission.location.serviceStatus.isEnabled) {
+      var status = await Permission.location.status;
+      if (status.isGranted) {
+        hasPermission = true;
+      } else {
+        Permission.location.request().then((value) {
+          setState(() {
+            hasPermission = (value == PermissionStatus.granted);
+          });
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Qiblah Finder',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+          builder: (context, snapshot) {
+            if (hasPermission) {
+              return const QiblaScreen();
+            } else {
+              return const Scaffold(
+                backgroundColor: Color.fromARGB(255, 48, 48, 48),
+              );
+            }
+          },
+          future: getPermission(),
+        ));
+  }
+}
